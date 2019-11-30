bdab662aa9aae971505f0ad3b1d5df3cbbdfeb24
Adriano Avelino <adriano-st777@hotmail.com>
2019-11-30 15:24:42 -0300

    adds tests to log in scenario

--- features/authentication.feature
+++ features/authentication.feature
@@ -9,3 +9,9 @@ Scenario: Signing up
   When I fill in the sign up form
   And I confirm the email
   Then Should see that my account is confirmed
+
+Scenario: User Logs in
+  Given I am a registered user
+  And I visit the homepage
+  When I fill in the loggin form
+  Then I should be logged in
--- features/step_definitions/authentication.rb
+++ features/step_definitions/authentication.rb
@@ -23,3 +23,20 @@ Then("Should see that my account is confirmed") do
 
   expect(page).to have_content(message)
 end
+
+Given("I am a registered user") do
+  @registered_user = FactoryBot.create(:user,
+                                      :email => "tester@testdomain.test",
+                                      :password => "pa$$word")
+end
+
+When("I fill in the loggin form") do
+  fill_in "user_email", :with => "tester@testdomain.test"
+  fill_in "user_password", :with => "pa$$word"
+
+  click_button "Log in"
+end
+
+Then("I should be logged in") do
+  expect(page).to have_content("Signed in successfully")
+end
